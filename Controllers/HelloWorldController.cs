94f5a48abad99af84643c7c9f425934dba700d3d
tanqinyong <tqinyong@yahoo.com.sg>
2024-05-14 15:24:31 +0800

    Initial commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace HIStest.Controllers
+
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        // localhost:7245/HelloWorld/
+        // Default method
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        // localhost:7245/HelloWorld/Welcome/3?name=rick
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int ID = 1)
+        {
+            // HTMLEncoder prevents XSS, do use this when displaying input back to user!
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
